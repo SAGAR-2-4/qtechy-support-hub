da7d76bf6881893a874b75349d9c2e6c75562d18
Sagar <shanmugalingamnithiyasagar@gmail.com>
2026-06-05 15:36:48 +0530

    Implement JWT authentication and user registration

---
+++ Backend/src/services/authService.js
@@ -0,0 +1,66 @@
+const User = require("../models/User");
+const generateToken = require("../utils/generateToken");
+
+const registerUser = async ({name, email, password}) => {
+    const existingUser= await User.findOne({ email });
+
+    if (existingUser) {
+        const error = new Error('Email already in use');
+        error.statusCode = 400;
+        throw error;
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role: "user",
+    });
+
+    const token = generateToken(user._id, user.role);
+
+    return {
+        user:{
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+        },
+        token,
+        };
+    };
+
+const loginUser = async ({ email, password }) => {
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.matchPassword(password))) {
+        const error = new Error('Invalid email or password');
+        error.statusCode = 401;
+        throw error;
+    }
+    
+    const isPasswordMatch = await user.matchPassword(password);
+
+    if (!isPasswordMatch) {
+        const error = new Error('Invalid email or password');
+        error.statusCode = 401;
+        throw error;
+    }
+
+    const token = generateToken(user._id, user.role);
+
+    return {
+        user:{
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+        },
+        token,
+    };
+};
+
+module.exports = {
+    registerUser,
+    loginUser,
+};
